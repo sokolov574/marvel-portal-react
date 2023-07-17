a20bf5e1434f3e8edf7a556bb6bf18952ee09f74
sokolov574 <sokolovloveoleg@gmail.com>
2023-07-17 13:21:56 +0200

    add randomChar list

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,10 @@
 
 
 class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=c50a2efc757f14d5a3ba3a953b895ba1';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -12,7 +16,11 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=c50a2efc757f14d5a3ba3a953b895ba1');
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
     }
 }
 
